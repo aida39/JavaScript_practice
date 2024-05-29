78abc57a76bce75f44e484a567d0d7122b9c9603
aida <alma2013mtk@gmail.com>
2024-05-29 09:32:15 +0900

    スケジューリングの学習中

---
+++ js/main.js
@@ -0,0 +1,27 @@
+// スケジューリング
+
+// let number = 1;
+
+// function timer() {
+//   console.log(number);
+//   number++;
+// }
+// setInterval(timer, 1000);
+
+
+// ダイアログ
+// const number = prompt("数字を入力してください");
+
+// const check = confirm("入力した数字は" + number + "ですね？");
+
+// function number_sort(number) {
+//   if(number % 2 === 0) alert("偶数です");
+//   else if (number % 2 === 1) alert("奇数です");
+//   else alert("入力された値が不正です");
+// }
+
+// if (check) {
+//   number_sort(number);
+// } else {
+//   alert("リロードしてやり直してください");
+// }
